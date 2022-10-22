f22f130e4579da93933f699c885a173ae75abe76
Besliu-Gherghescu-Andrei-Alexandru <114858086+Besliu-Gherghescu-Andrei-Alexandru@users.noreply.github.com>
2022-10-23 00:06:44 +0300

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,9 @@
+let fullName = 'Besliu Andrei'
+let isRaining = true
+let planet = 'Earth'
+let temperature = 30
+temperature = temperature + 2
+console.log(temperature)
+let celsius = 10
+let fahrenheit = celsius * 1.8 + 32
+console.log(fahrenheit)
